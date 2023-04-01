7b24c75b8c111b8800c1f7e96592f5d9ce768e32
Melanie Becker <becker.mel@northeastern.edu>
2023-04-01 16:52:22 -0400

    Added conditional for where data should be looked for depending on where I am viewing the site

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-const TUITS_API = 'https://tuiter-node-server-app-cs96.onrender.com/api/tuits';
+const API_BASE = process.env.REACT_APP_API_BASE;
+const TUITS_API = `${API_BASE}/tuits`;
 
 export const findTuits  = async () => {
     const response = await axios.get(TUITS_API);
