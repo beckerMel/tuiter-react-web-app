1c14d909d8ea7aa692ea8590abc791405725f602
Melanie Becker <becker.mel@northeastern.edu>
2023-04-01 17:34:37 -0400

    hw done!

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
-const API_BASE = process.env.REACT_APP_API_BASE;
-console.log("API base is" + process.env.REACT_APP_API_BASE);
+const API_BASE = process.env.REACT_APP_API_BASE
 const TUITS_API = `${API_BASE}/tuits`;
 
 export const findTuits  = async () => {
